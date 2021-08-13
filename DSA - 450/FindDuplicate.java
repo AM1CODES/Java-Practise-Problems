78d01a4b15a8aa23791875b6571bff563811bc58
Aayush Mishra <aayush.mishra@gmail.com>
2021-08-13 18:10:02 +0530

    FindDuplicate

---
+++ DSA - 450/FindDuplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int store = 0;
+        Arrays.sort(nums);
+        for(int i = 0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                store = nums[i];
+            }
+        }
+        return store;
+    }
+}
