8e189bad1e7fb329f78945942409e5b80973e8a4
Aayush Mishra <aayush.mishra@gmail.com>
2021-10-02 11:29:34 +0530

    Pattern 3

---
+++ LeetCode/Pattern/Pattern3.java
@@ -0,0 +1,13 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n+1-row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+}
+}
